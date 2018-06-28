4d162087589264a3085673bb1f6e4f30eee1b1e0
Ben Elliot <belliot@liveperson.com>
2018-06-28 15:53:34 +1000

    Added collection of playlist selection, have some helper methods to make presentation, collection and validation easier

--- spotsort.py
+++ spotsort.py
@@ -4,45 +4,43 @@ from tabulate import tabulate
 
 
 class SpotSort:
+    presented_to_user = []
+    selected_playlists = []
 
     def __init__(self, spotify_client):
         self.spotify_client = spotify_client
         self.user_profile = self.spotify_client.get_user_profile()
 
-    def __
-
-    def parse_string_list(self, string):
-        parsed = [x.strip() for x in string.split(',')]
-        for selection in parsed:
-            if selection not in valid_values:
-                raise click.BadParameter(f"Selected an invalid playlist: {selection}", param=string)
+    def __print_tabulated_data(self, pre_string,post_string, tabulated_data, tabulated_headers):
+        self.present_to_user = tabulated_data
+        print(pre_string)
+        print(tabulate(tabulated_data, headers=tabulated_headers))
+        print(post_string)
         return
 
+    def __parse_string_list(self, string):
+        parsed = [int(x.strip()) for x in string.split(',')]
+        print(parsed)
+        valid = [x[0] for x in self.present_to_user]
+        print(valid)
+        for selection in parsed:
+            if selection not in valid:
+                raise click.BadParameter(f"Selected an invalid playlist: {selection}", param=string)
+        return parsed
+
     def get_playlist_selection(self, playlists):
-        # Prepare the data for printing with
-        headers = ['ID', 'Name', 'Tracks', 'PID']
-        tabular = [[i, x['name'], x['tracks']['total'], x['id']] for i,x in enumerate(playlists) if x['tracks']['total'] > 0]
+        # Prepare the data for printing with tabulate
+        playlists_to_present_headers = ['ID', 'Name', 'Tracks', 'PID']
+        playlists_to_present = [[i, x['name'], x['tracks']['total'], x['id']] for i,x in enumerate(playlists) if x['tracks']['total'] > 0]
 
-        print("Available Playlists")
-        print(tabulate(tabular, headers=headers))
-        print('Select the playlists to add to the session.')
-        value = click.prompt('Enter the playlists, using commas to separate the IDs', value_proc=self.parse_string_list)
-        
-        test_string = '1,5,7,10,20,40,42,58'
-        test = self.parse_string_list(test_string, [x[0] for x in tabular])
+        self.__print_tabulated_data(
+            'Available Playlists',
+            'Select the playlists to add to the session.',
+            playlists_to_present,
+            playlists_to_present_headers)
+        selected_indexes = click.prompt('Enter the playlists, using commas to separate the IDs', value_proc=self.__parse_string_list)
     
-        # selected = []
-        # print("Entry was: ", value)
-        # for entry in value:
-        #     selected.append(int(entry) - 1)
-        
-        # headers = ['Name', 'Tracks']
-        # tabular = []
-        # for select in selected:
-        #     tabular.append([playlists[select]['name'], playlists[select]['tracks']['total'] ])
-
-        # print("Selected Playlists")
-        # print(tabulate(tabular, headers=headers))
+        return [playlists[x] for x in selected_indexes]
 
 
 @click.command()
