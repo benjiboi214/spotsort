b2658ec844840d3b996d5c9b345002d53447338e
Ben Elliot <benjiboi214@gmail.com>
2018-06-29 08:36:20 +1000

    Added django orm settings to settings.py

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+# Spotify App Developer Details
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 SPOTIFY_REDIRECT_URI = 'http://localhost:8888/callback'
@@ -11,4 +12,32 @@ SPOTIFY_REQUIRED_API_SCOPES = [
     'playlist-read-private'  # To get the full list of available playlists to navigate through.
 ]
 
+# Default Spotify User if not specified on CLI
 DEFAULT_USER = 'benjiboi214'
+
+
+## Djangpo ORM Settings
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '!ll2kn4tq9+xyz&ahyy(v_5ag0p#dptvbt1#p6fsw177n$+_0s'
+
+INSTALLED_APPS = [
+    'data'
+]
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
