feeba3f2666a84bd3e5878f07dd4eafec3a9b293
Ben Elliot <benjiboi214@gmail.com>
2018-07-02 08:37:38 +1000

    Changed initial migrations.

--- data/models.py
+++ data/models.py
@@ -2,7 +2,7 @@ import sys
 
 try:
     from django.db import models
-except  Exception:
+except Exception:
     print("There was an error loading django modules. Do you have django installed?")
     sys.exit()
 
